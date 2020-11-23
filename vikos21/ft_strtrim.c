b33a491586cde3b93236e7b33af41c01d97768f2
Tennie Charissa <tchariss@mi-f5.msk.21-school.ru>
2020-11-23 12:54:53 +0300

    ustal

--- vikos21/ft_strtrim.c
+++ vikos21/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tchariss <tchariss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 13:21:31 by tchariss          #+#    #+#             */
-/*   Updated: 2020/11/18 19:48:12 by tchariss         ###   ########.fr       */
+/*   Updated: 2020/11/23 12:52:41 by tchariss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 Если у set нет пробелов в начале или в конце, функция возвращает копию s1. 
 Если распределение завершается неудачно, функция возвращает NULL
 */
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	a;
 	size_t	len;
 	size_t	b;
-	char *trim;
+	char	*trim;
 
 	if (s1 == NULL || set == NULL)
 		return (NULL); // защищаю функцию
@@ -51,15 +52,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 		while (search(set, s1[a]) == 1 && s1[a] != '\0')
 			a++;
 	}
-	trim = malloc(sizeof(char) * ((len - a) + 1));
-	if (trim == NULL)
+	if (!(trim = malloc(sizeof(char) * ((len - a) + 1))))
 		return (NULL);
 	while (a < len)
-	{
-		trim[b] = s1[a];
-		b++;
-		a++;
-	}
+		trim[b++] = s1[a++];
 	trim[b] = '\0';
 	return (trim);
+}
+
+
+int	main()
+{
+	char s1[] = "          ";
+	printf("%s\n", ft_strtrim( s1 , " "));
+	return (0);
 }
